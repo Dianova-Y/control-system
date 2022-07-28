c32ffd6a43556a19d2d3401a03043d11ecc6751f
Yulia Dianova <dianova@internet.ru>
2022-07-28 13:29:19 +0300

    Добавлено обрезанное среднее

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class SalesManager {
     protected int[] sales;
 
@@ -13,5 +15,20 @@ public class SalesManager {
             }
         }
         return max;
+    }
+
+    public int middle(){
+        int min = 100000;
+        int sum = 0;
+        for (int sale : sales) {
+            if (sale < min) {
+                min = sale;
+            }
+        }
+        for (int i = 0; i < sales.length; i++) {
+            sum +=sales[i];
+        }
+        int middle = (sum - min - max()) / (sales.length - 2);
+        return middle;
     }
 }
